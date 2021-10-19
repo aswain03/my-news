d8b46e167c8501d6b0489702547752d39efd3a18
Alexander Swain <alexander.swain03@gmail.com>
2021-10-19 21:20:13 +0100

    add set up of header and css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../styles/Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <p>Header</p>
+      <section className="header_left">
+        <Link to="/">
+          <img
+            src="https://as2.ftcdn.net/v2/jpg/01/38/69/89/500_F_138698989_ekcoWNH5KmFNmTqjJl0vlHtp9Yvm8aXd.jpg"
+            alt=""
+            className="header_logo"
+          />{" "}
+        </Link>
+      </section>
+      <section className="header_centre">
+        <h1 className="header_title">FAKE NEWS</h1>
+      </section>
+      <section className="header_right">
+        <Link to="www.linkedin.com/in/alexander-swain">
+          <i id="header_linkedin" class="fab fa-linkedin"></i>
+        </Link>
+      </section>
     </div>
   );
 };
